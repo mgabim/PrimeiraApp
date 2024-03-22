31379b3a39d6311436c54979b3268a9d119ee47d
gabi <xucagabi22@gmail.com>
2024-03-22 09:09:47 -0300

    coloquei comentarios

--- app/src/main/java/machado/maria/primeiraapp/MainActivity.java
+++ app/src/main/java/machado/maria/primeiraapp/MainActivity.java
@@ -13,16 +13,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar); //cria uma variavel e armazena o botao btnenviar da interface
+        setContentView(R.layout.activity_main); //le o arquivo de layout e constroi a interface
+        Button btnEnviar = findViewById(R.id.btnEnviar); //cria uma variavel e armazena o botao btnEnviar da interface
+
         btnEnviar.setOnClickListener(new View.OnClickListener() { //metodo pra saber quando o usuario clica no botao
             @Override
             public void onClick(View v) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //cria uma variavel e armazena o edittext etdigiteaqui
-                String textoDigitado = etDigiteAqui.getText().toString(); //cria uma variavel e armazena o que foi escrito no etdigiteaqui
-                Intent i = new Intent(MainActivity.this, NextActivity.class); //
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //cria uma variavel e armazena o edittext etDigiteAqui
+                String textoDigitado = etDigiteAqui.getText().toString(); //cria outra variavel e armazena o que foi escrito no etDigiteAqui
+                Intent i = new Intent(MainActivity.this, NextActivity.class); //cria uma variavel Intent pra navegar pra proxima tela
+                i.putExtra("texto", textoDigitado); //leva junto o texto armazenado na variavel textoDigitado
+                startActivity(i); //navega pra proxima tela
             }
         });
     }
--- app/src/main/java/machado/maria/primeiraapp/NextActivity.java
+++ app/src/main/java/machado/maria/primeiraapp/NextActivity.java
@@ -11,11 +11,11 @@ public class NextActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next);
+        setContentView(R.layout.activity_next); //le o arquivo de layout e constroi a interface
 
-        Intent i = getIntent();
-        String texto = i.getStringExtra("texto");
-        TextView tvTexto = findViewById(R.id.tvTexto);
-        tvTexto.setText(texto);
+        Intent i = getIntent(); //pega o Intent enviado pela tela MainActivity
+        String texto = i.getStringExtra("texto"); //pega o texto que veio junto com a Intent
+        TextView tvTexto = findViewById(R.id.tvTexto); //cria uma variavel e armazena o textview
+        tvTexto.setText(texto); //bota o texto que veio da outra tela na textview dessa interface
     }
 }
